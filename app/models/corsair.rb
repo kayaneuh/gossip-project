1f42e54f820f53f213a7047e96840fee07f9c790
kayaneuh <n.kaliane@gmail.com>
2017-08-08 16:46:12 +0200

    liaison entre gossip et corsair qui ne fonctionne pas

--- app/models/corsair.rb
+++ app/models/corsair.rb
@@ -3,4 +3,5 @@ class Corsair < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :gossips
 end
